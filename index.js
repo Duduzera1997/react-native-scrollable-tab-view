d5f7a780c2511606782112c45e381754ad89c4d5
Eduardo Jesus do Nascimento <eduardo.nascimento@neon.com.br>
2022-03-04 12:02:21 -0300

    fix: change repo to get react-native-pager-view

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ const {
 } = ReactNative;
 
 const TimerMixin = require('react-timer-mixin');
-const ViewPager = require('react-native-pager-view');
+import PagerView from 'react-native-pager-view';
 
 const SceneComponent = require('./SceneComponent');
 const DefaultTabBar = require('./DefaultTabBar');
 const ScrollableTabBar = require('./ScrollableTabBar');
 
 const AnimatedViewPagerAndroid = Platform.OS === 'android' ?
-  Animated.createAnimatedComponent(ViewPager) :
+  Animated.createAnimatedComponent(PagerView) :
   undefined;
 
 const ScrollableTabView = createReactClass({
